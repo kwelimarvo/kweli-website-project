8c1dcc071a1f07a56900d7b990329ff3daae3145
kwelimarvo <marvin@kwelicapital.com>
2022-06-15 09:17:49 +0300

    Project Installation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-neutral-900 pb-10">
+
+    <div>
+      <h1 class="text-white text-xl opacity-100 font-medium text-center py-16">BRAND PARTNERS</h1>
+    </div>  
+
+    <div class="flex justify-center space-x-16">
+      <span class="text-white my-4 font-bold text-base"><img src="../assets/images/partners/genghis.png" alt="">Fund Manager</span>
+      <img src="../assets/images/partners/line.png" alt="">
+      <span class="text-white my-4 font-bold text-base"><img src="../assets/images/partners/stanbic.png" alt="">Funds Trustee & Custodian</span>
+    </div>
+    
+  </div>
+
+  <section id="investor">
+    <h2 class="h-8 text-neutral-900 text-2xl font-semibold sm:text-left md:text-center mt-20">Investor Relations</h2>
+  </section>
+
+  <section id="sustainability" class="bg-neutral-100 mt-3 py-16">
+    <h3 class="text-neutral-900 font-semibold text-2xl">Sustainability</h3>
+    <span class="uppercase text-xs font-normal">Using innovation for good</span>
+    
+    <div class="text-left text-sm w-80 mt-6">
+      <p>We strongly believe that the success of many private and public initiatives requires strong partnerships with private companies and governments 
+        in Africa.
+      </p>
+      <br>
+      <p>
+         Our data-driven solutions have allowed us to form strong partnerships in both government & the private sector.
+      </p>
+    </div>
+  </section>
+
+  <section id="mission">
+    <div class="text-center mb-10">
+      <h3 class="text-black text-2xl font-semibold mt-10">Our Mission</h3>
+      <p class="text-zinc-500 italic text-xl font-semibold"><span class="text-9xl text-center font-merriweather z-1">“</span>To provide the public & private sectors with bespoke, data-driven ICT solutions that inform their strategic decisions.<span class="text-9xl text-center font-merriweather z-1">“</span></p>
+    </div>
+
+    <div>
+      <div></div>
+      <div>
+        <p class="uppercase text-xl font-bold text-neutral-900">Let's build something great.<br>together.</p>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="footer" class="bg-[url('/src/assets/images/footerimg.png')]">
+    <div>
+      <p>&copy; Copyright KWELI CAPITAL. All Rights Reserved</p>
+    </div>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'lato': ['Lato'],
+        'garamond': ['Garamond'],
+        'merriweather': ['Merriweather'],
+    }
+    },
+  },
+  plugins: [],
+}
